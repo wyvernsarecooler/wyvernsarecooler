d5c25f141759aeed7ebb3adc89464c6238f9bb7e
Simon <93844016+wyvernsarecooler@users.noreply.github.com>
2022-05-02 17:48:40 +0200

    Update helpme.cpp

--- helpme.cpp
+++ helpme.cpp
@@ -1,5 +1,28 @@
-if (happiness == NULL || false) {
-	life.struggle;
-} else {
-	life.struggle;
+#include <iostream>
+
+class myLife {
+private:
+	bool happiness;
+	int health;
+public:
+	myLife(bool x, int y) {
+		happiness = x;
+		health = y;
+	};
+	~myLife();
+	
+	void struggle() {
+		std::cout << "pain"
+	};
+};
+	
+
+int main(void) {
+	myLife life(false, 0);
+
+	if (happiness == NULL || false) {
+		life.struggle();
+	} else {
+		life.struggle();
+	}
 }
