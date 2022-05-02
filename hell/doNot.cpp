e215415bd5ce5c83aebc9e0dc13ae6751f3dc035
Simon <93844016+wyvernsarecooler@users.noreply.github.com>
2022-05-02 22:17:31 +0200

    WAAAA
    i am sorry

---
+++ hell/doNot.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+#define 🌜int
+#define 🔨 double
+#define 🧵 char
+#define 🧱 const
+#define 👈 <
+#define 👉 >
+#define 💀 &&
+#define ⚠️ ||
+#define 🤷 if
+#define ➰ while
+#define 4️⃣ for
+#define 😴 void
+#define 😵‍💫 ++
+#define 🔣 false
+#define 😎 ==
+
+
+class Console {
+public:
+	template <typename x>
+	void log(x const &value) {
+		std::cout << value << std::endl;
+	};
+};
+
+🌜main(😴) {
+	Console console;
+	🧱🧵* 😁 = "pure evil";
+	🌜🤯 = 4;
+	🌜🐾 = 3;
+	🔨😘 = 3.21;
+	
+		4️⃣(🌜ℹ️; ℹ️ 👈 🤯; ℹ️😵‍💫) {
+			➰(🔣) {
+				🤷‍(😘 👉 🤯 💀 😘 😎 🐾 ⚠️ 🔣) {
+					console.log(😁);
+				}
+			}
+		}
+}
