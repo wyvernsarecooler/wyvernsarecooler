812ef2cd5de3f705084e008b98d63d539574a846
Duck <104778038+duck2210@users.noreply.github.com>
2022-05-05 14:12:48 -0500

    Update and rename suisexo.py to suisexov2.py

--- suisexo.py
+++
@@ -1,8 +0,0 @@
-A = int(input("favorite sexual/romantic relationship age"))
-
-  if A >= 18:
-    print("safe")
-    else:
-      print("boone")
-
-      # duck you have no cs degrees smh
---
+++ suisexov2.py
@@ -0,0 +1,25 @@
+A = int(input("/romantic relationship age"))
+
+  while A != 0:
+    if A < 1:
+      break
+    elif A < 8:
+      age = "boone"
+      print((A, age,))
+    elif A < 12:
+      age = "karu probably"
+      print((A, age))
+     elif A < 16:
+      age = "questionable depending your age"
+      print((A, age))
+     elif A > 18:
+      age = "legal"
+      print((A, age))
+      else:
+        print("either legal, or unrealistic")
+        
+       A = int(input("introduce 0"))
+      
+    
+
+      # duck you have no cs degrees smh
