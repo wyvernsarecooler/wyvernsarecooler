60af64037ee19532fcc92151738f146fb3258fee
Simon <93844016+wyvernsarecooler@users.noreply.github.com>
2022-06-27 20:33:30 +0200

    i know you love it theo

---
+++ arrowOperator.c
@@ -0,0 +1,18 @@
+#include <stdio.h> 
+#include <stdlib.h>
+
+struct arrowOp {
+    const char* theosOpinion;
+};
+
+struct arrowOp* theo = NULL;
+
+int main(int argc, char *argv[]) {
+    theo = (struct arrowOp*)
+        malloc(sizeof(struct arrowOp));
+
+    theo->theosOpinion = "theo loves the arrow operator! (scope resolution is also an operator)";
+
+    printf("%d", theo->theosOpinion);
+  return 0;
+}
